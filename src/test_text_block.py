f27763a0894e5e62764ea4c2c036f9daa8209e94
Urs Betschart <urszitrus@gmail.com>
2026-09-18 07:25:52 +0200

    add block to blocktype function

--- src/test_text_block.py
+++ src/test_text_block.py
@@ -1,6 +1,6 @@
 import unittest
 
-from text_block import markdown_to_blocks
+from text_block import BlockType, markdown_to_blocks, block_to_block_type
 
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -26,7 +26,7 @@ class TestMarkdownToBlocks(unittest.TestCase):
 
         self.assertListEqual(expected, result)
 
-    def test_markdown_to_blocks(self):
+    def test_markdown_to_blocks2(self):
         md = """
 This is **bolded** paragraph
 
@@ -45,3 +45,100 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_to_block_type_different_line_between(self):
+        block = """> This starts like a quote
+but this line does not
+> this one does again"""
+
+        result = block_to_block_type(block)
+
+        expected = BlockType.PARAGRAPH
+
+        self.assertEqual(expected, result)
+
+    def test_block_to_block_type_empty(self):
+        block = ""
+
+        result = block_to_block_type(block)
+
+        expected = BlockType.PARAGRAPH
+
+        self.assertEqual(expected, result)
+
+    def test_block_to_block_type_ordered_list(self):
+        block = """1. First item
+2. Second item
+3. Third item
+4. Fourth item"""
+
+        result = block_to_block_type(block)
+
+        expected = BlockType.ORDERED_LIST
+
+        self.assertEqual(expected, result)
+
+    def test_block_to_block_type_false_ordered_list(self):
+        block = """1. First item
+2. Second item
+4. Fourth item"""
+
+        result = block_to_block_type(block)
+
+        expected = BlockType.PARAGRAPH
+
+        self.assertEqual(expected, result)
+
+
+    def test_block_to_block_type_heading(self):
+        block = """### Example heading"""
+
+        result = block_to_block_type(block)
+
+        expected = BlockType.HEADING
+
+        self.assertEqual(expected, result)
+
+    def test_block_to_block_type_code(self):
+        block = """```
+print("hello")
+print("world")
+```"""
+
+        result = block_to_block_type(block)
+
+        expected = BlockType.CODE
+
+        self.assertEqual(expected, result)
+
+    def test_block_to_block_type_quote(self):
+        block = """> First quote line
+> Second quote line
+> Third quote line"""
+
+        result = block_to_block_type(block)
+
+        expected = BlockType.QUOTE
+
+        self.assertEqual(expected, result)
+
+    def test_block_to_block_type_unordered_list(self):
+        block = """- First item
+- Second item
+- Third item"""
+
+        result = block_to_block_type(block)
+
+        expected = BlockType.UNORDERED_LIST
+
+        self.assertEqual(expected, result)
+
+    def test_block_to_block_type_paragraph(self):
+        block = """This is just a normal paragraph
+with a second line."""
+
+        result = block_to_block_type(block)
+
+        expected = BlockType.PARAGRAPH
+
+        self.assertEqual(expected, result)
--- src/text_block.py
+++ src/text_block.py
@@ -1,3 +1,7 @@
+from enum import Enum
+
+import re
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     block_list = []
@@ -6,3 +10,44 @@ def markdown_to_blocks(markdown):
         if block:
             block_list.append(block)
     return block_list
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+def block_to_block_type(markdown_block) -> BlockType:
+    if not markdown_block:
+        return BlockType.PARAGRAPH
+
+    lines = markdown_block.splitlines()
+
+    if markdown_block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    
+    if markdown_block.startswith("```\n") and markdown_block.endswith("```"):
+        return BlockType.CODE
+
+    if all(line.startswith(">") for line in lines):
+        return BlockType.QUOTE
+
+    if all(line.startswith("- ") for line in lines):
+        return BlockType.UNORDERED_LIST
+
+    is_ordered = True
+
+    for index, line in enumerate(lines):
+        expected = f"{index + 1}. "
+
+        if not line.startswith(expected):
+            is_ordered = False
+            break
+
+    if is_ordered:
+        return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
+    
